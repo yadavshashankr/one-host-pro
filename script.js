77929d3b831fd7dcbbc9fa69e88b877ccd792534
Shashank Yadav <81215694+yadavshashankr@users.noreply.github.com>
2025-07-24 04:14:07 +0530

    Add Open Relay Project TURN server to PeerJS config for reliable internet connections

--- script.js
+++ script.js
@@ -405,7 +405,12 @@ function initPeerJS() {
             config: {
                 iceServers: [
                     { urls: 'stun:stun.l.google.com:19302' },
-                    { urls: 'stun:global.stun.twilio.com:3478' }
+                    { urls: 'stun:global.stun.twilio.com:3478' },
+                    {
+                        urls: 'turn:openrelay.metered.ca:80',
+                        username: 'openrelayproject',
+                        credential: 'openrelayproject'
+                    }
                 ]
             }
         });
